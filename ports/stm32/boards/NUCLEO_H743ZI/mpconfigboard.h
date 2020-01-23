af5e8741d8d55dffcb38aebb0fe412a182d442e7
Wojciech Zabołotny <wzab@wzdell.nasz.dom>
2020-01-23 10:40:02 +0100

    Switched off VUSB sensing due to buggy voltage divider.

--- ports/stm32/boards/NUCLEO_H743ZI/mpconfigboard.h
+++ ports/stm32/boards/NUCLEO_H743ZI/mpconfigboard.h
@@ -78,8 +78,8 @@ void NUCLEO_H743ZI_board_early_init(void);
 
 // USB config
 #define MICROPY_HW_USB_FS              (1)
-#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
-#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
+//#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
+//#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
 
 // FDCAN bus
 #define MICROPY_HW_CAN1_NAME  "FDCAN1"
